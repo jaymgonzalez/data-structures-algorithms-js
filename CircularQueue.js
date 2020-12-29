d44b00754fc74c873012417abe1427ccb1b59b24
Jaime Munoz <jaimungon@gmail.com>
2020-12-29 17:28:21 +0100

    add queues implementation

---
+++ CircularQueue.js
@@ -0,0 +1,22 @@
+const Queue = require('./Queues.js')
+
+function CircularQueue(nameList, num) {
+  let queue = new Queue()
+  for (let i = 0; i < nameList.length; i++) {
+    queue.enqueue(nameList[i])
+  }
+  let eliminated = ''
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue())
+    }
+    eliminated = queue.dequeue()
+    console.log(eliminated + ' was eliminated from the Hot Potato game.')
+  }
+  return queue.dequeue()
+}
+
+
+let names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl']
+let winner = CircularQueue(names, 7)
+console.log('The winner is: ' + winner)
---
+++ Queues.js
@@ -0,0 +1,44 @@
+function Queue() {
+
+  let items = []
+
+  this.enqueue = function (element) {
+    items.push(element)
+  }
+
+  this.dequeue = function () {
+    return items.shift()
+  }
+
+  this.front = function () {
+    return items[0]
+  }
+
+  this.isEmpty = function () {
+    return items.length == 0
+  }
+
+  this.size = function () {
+    return items.length
+  }
+
+  this.print = function () {
+    console.log(items.toString())
+  }
+
+}
+
+module.exports = Queue
+
+// let queue = new Queue()
+// console.log(queue.isEmpty())
+// queue.enqueue("Jay")
+// queue.enqueue("Debs")
+// queue.enqueue("Marinella")
+// queue.enqueue("Giomaria")
+// queue.print()
+// console.log(queue.size())
+// console.log(queue.isEmpty())
+// queue.dequeue()
+// queue.dequeue()
+// queue.print()
